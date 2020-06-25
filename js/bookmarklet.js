7c3cb4a3da836713fc098872f1fb85a2cc2224cf
Ric Bevan <bevoid@icloud.com>
2020-06-25 13:06:57 +0100

    Update bookmarklet.js

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,2 +1,2 @@
-document.body.appendChild(document.createElement('script')).src='https://ricbevan.github.io/office/js/gbc-office.js?' + new Date().getTime();
-document.body.appendChild(document.createElement('script')).src='https://ricbevan.github.io/office/js/x2m.js?' + new Date().getTime();
+document.body.appendChild(document.createElement('script')).src='https://ricbevan.github.io/gbc-office/js/gbc-office.js?' + new Date().getTime();
+document.body.appendChild(document.createElement('script')).src='https://ricbevan.github.io/gbc-office/js/x2m.js?' + new Date().getTime();

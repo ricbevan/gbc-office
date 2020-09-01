7ee62a0a48500bbd02a1c086cbc1564f6b7a2349
Ric Bevan <bevoid@icloud.com>
2020-09-01 11:04:30 +0100

    Update month-invoices.js

--- js/month-invoices.js
+++ js/month-invoices.js
@@ -3,18 +3,16 @@ var date = new Date();
 var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
 var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-function redirect() {
-  if (!thisMonth) {
-    firstDay.setMonth(firstDay.getMonth() + 1);
-    lastDay.setMonth(lastDay.getMonth() + 1);
-  }
-
-  console.log('https://go.xero.com/AccountsReceivable/Search.aspx?invoiceStatus=INVOICESTATUS%2fAUTHORISED&graphSearch=False&startDate=' +
-    formatDate(firstDay) +
-    '&endDate=' +
-    formatDate(lastDay) +
-    '&dateWithin=invoice&pageSize=25&orderBy=PaidToName&direction=ASC&unsentOnly=False');
+if (!thisMonth) {
+  firstDay.setMonth(firstDay.getMonth() + 1);
+  lastDay.setMonth(lastDay.getMonth() + 1);
 }
+
+console.log('https://go.xero.com/AccountsReceivable/Search.aspx?invoiceStatus=INVOICESTATUS%2fAUTHORISED&graphSearch=False&startDate=' +
+  formatDate(firstDay) +
+  '&endDate=' +
+  formatDate(lastDay) +
+  '&dateWithin=invoice&pageSize=25&orderBy=PaidToName&direction=ASC&unsentOnly=False');
 
 function formatDate(date) {
   var d = new Date(date),
@@ -33,4 +31,3 @@ function formatDate(date) {
 
 // document.body.appendChild(document.createElement('script')).src='https://ricbevan.github.io/gbc-office/js/month-invoices.js?' + new Date().getTime();
 // thisMonth = false;
-// redirect();

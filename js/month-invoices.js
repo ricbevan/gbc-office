db4b44710e17f844b48178d1836685d66c33219c
Ric Bevan <bevoid@icloud.com>
2020-09-01 11:07:47 +0100

    Update month-invoices.js

--- js/month-invoices.js
+++ js/month-invoices.js
@@ -1,4 +1,4 @@
-var thisMonth = true;
+var thisMonth;
 var date = new Date();
 var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
 var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
